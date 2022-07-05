e5c765c0a0cfe4084ff35baaba038757fb618938
GageVanK <gagevank@gmail.com>
2022-07-04 23:26:16 -0400

    Added Junit Tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,10 +3,21 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class AppTest {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void FibEquals() {
+		int val = 10;
+		
+		assertEquals(55, App.fibMethod(val));
+		
+	}
+	
+	@Test
+	public void FibEqualsFail() {
+		int val = 10;
+		
+		assertEquals(10, App.fibMethod(val));
+		
 	}
 
 }
